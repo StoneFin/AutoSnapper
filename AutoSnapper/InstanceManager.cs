0dd68b436adcb5d6f26a72760b29b5254042661f
Jesse Temple <jesse@stonefin.com>
2014-08-07 16:11:29 -0500

    got instance manager set up initially, need config and command line argument support for instances

---
+++ AutoSnapper/InstanceManager.cs
@@ -0,0 +1,50 @@
+﻿using Amazon;
+using Amazon.EC2;
+using Amazon.EC2.Model;
+using Amazon.S3;
+using Amazon.SimpleDB;
+using Amazon.SimpleDB.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+namespace AutoSnapper
+{
+  class InstanceManager
+  {
+    /// <summary>
+    /// stops instances whos InstanceIds are contained in instanceIds
+    /// </summary>
+    /// <param name="instanceIds"></param>
+    /// <returns></returns>
+    public static string StopInstances(List<string> instanceIds)
+    {
+      var sb = new StringBuilder();
+
+      using (var sr = new StringWriter(sb))
+      {
+        var instanceCsv = String.Join(", ", instanceIds);
+
+        sr.WriteLine("===========================================");
+        sr.WriteLine(string.Format("Stopping EC2 Instances: {0}", instanceCsv));
+        sr.WriteLine("===========================================");
+
+        var stopInstancesRequest = new StopInstancesRequest()
+        {
+          InstanceIds = instanceIds,
+          Force = false
+        };
+
+        var ec2Client = Services.GetEc2Client();
+
+        ec2Client.StopInstances(stopInstancesRequest);
+      }
+
+      return sb.ToString();
+    }
+  }
+}
--- AutoSnapper/Program.cs
+++ AutoSnapper/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AutoSnapper
 {
@@ -9,7 +10,11 @@ namespace AutoSnapper
       Console.Write(Services.GetServiceOutput());
       Console.Write(SnapshotManager.CreateSnapshotAllVolumes());
       Console.Write(SnapshotManager.PurgeOldSnapshots());
-      
+
+      //TODO: add command line args to specify this, or put it into the config, or whatever
+      var instanceIds = new List<string>();
+
+      Console.Write(InstanceManager.StopInstances(instanceIds));
 #if DEBUG
       //stops the console so you can read the output for testing purposes
       Console.Write("press enter to continue...");
