7b25a5548af727b812865e10585b1dfb30c85d75
Gabe Williamson <gabe@stonefin.com>
2015-07-06 13:32:39 -0500

    add fluent, refactor to use command line args. Needs testing yet.

--- AutoSnapper/Program.cs
+++ AutoSnapper/Program.cs
@@ -1,52 +1,84 @@
 ﻿using System;
+using Fclp;
 
 namespace AutoSnapper
 {
-  class Program
+  public class Program
   {
-    public static void Main(string[] args)
-    {
-      Console.Write(Services.GetServiceOutput());
-
-      foreach (var arg in args)
-      {
-        if (arg.Equals("/snapshotVolumes"))
-        {
-          Console.Write(SnapshotManager.CreateSnapshotAllVolumes());
-          Console.Write(SnapshotManager.PurgeOldSnapshots());
+    public static void Main(string[] args) {
+      var pa = new FluentCommandLineParser();
+      var p = new FluentCommandLineParser<SnapperArguments> {IsCaseSensitive = false};
+      p.Setup<bool>(x => x.DisplaySummary).As('v', "verbose").WithDescription("Test Description");
+      p.SetupHelp("?", "help").Callback(Console.Write);
+      var result = p.Parse(args);
+      //display help and pause, or display errors and (if debug pause else exit) else run.
+      if (result.HelpCalled || result.EmptyArgs) {
+        p.HelpOption.ShowHelp(p.Options);
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+      }
+      else {
+        if (result.HasErrors) {
+          Console.WriteLine(result.ErrorText);
+          p.HelpOption.ShowHelp(p.Options);
+          pause();
         }
-
-        if (arg.Equals("/startInstances"))
-        {
-          var instancesToStart = Services.GetInstancesToStart();
-
-          Console.Write(InstanceManager.StartInstances(instancesToStart));
-          Console.Write("Starting instances, please wait... ");
-
-          //TODO: make this a threaded process, so we can sleep until the instances are ready
-
-          //wait here until all the started instances are running, can't assign IPs until then
-          while (!InstanceManager.InstancesAreRunning(instancesToStart))
-          {
-            Console.Write("... ");
-          }
-
-          Console.Write("\r\n");
-
-          Console.Write(InstanceManager.AssociateInstancesToElasticIps(instancesToStart));
-        }
-
-        if (arg.Equals("/stopInstances"))
-        {
-          Console.Write(InstanceManager.StopInstances(Services.GetInstancesToStop()));
+        else {
+          run(p.Object);
+          pause();
         }
       }
+    }
 
+    static void pause() {
 #if DEBUG
       //stops the console so you can read the output for testing purposes
       Console.Write("press enter to continue...");
       Console.Read();
 #endif
+    }
+
+    private static void SafeInvoke(Func<String> a) {
+      try {
+        Console.WriteLine(a());
+      }
+      catch (Exception ex) {
+        
+      }
+    }
+
+    private static void run(SnapperArguments args) {
+      if (args.DisplaySummary)
+        SafeInvoke(Services.GetServiceOutput);
+
+        if (args.SnapshotVolumes) {
+          SafeInvoke(SnapshotManager.CreateSnapshotAllVolumes);
+          SafeInvoke(SnapshotManager.PurgeOldSnapshots);
+        }
+
+        if (args.StartInstances) {
+          SafeInvoke(()=>
+          {
+            var instancesToStart = Services.GetInstancesToStart();
+
+            Console.Write(InstanceManager.StartInstances(instancesToStart));
+            Console.Write("Starting instances, please wait... ");
+
+            //TODO: make this a threaded process, so we can sleep until the instances are ready
+
+            //wait here until all the started instances are running, can't assign IPs until then
+            while (!InstanceManager.InstancesAreRunning(instancesToStart)) Console.Write("... ");
+
+            Console.Write("\r\nAssociating elastic ips....");
+
+            Console.Write(InstanceManager.AssociateInstancesToElasticIps(instancesToStart));
+            return "Done Starting.";
+          });
+        }
+
+        if (args.StopInstances) 
+          SafeInvoke(()=>InstanceManager.StopInstances(Services.GetInstancesToStop()));
+      
     }
   }
 }
---
+++ AutoSnapper/SnapperArguments.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoSnapper {
+  public class SnapperArguments
+  {
+    public bool SnapshotVolumes { get; set; }
+    public bool StartInstances { get; set; }
+    public bool StopInstances { get; set; }
+    public bool DisplaySummary { get; set; }
+  }
+}
