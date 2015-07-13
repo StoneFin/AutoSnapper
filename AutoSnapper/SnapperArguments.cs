07c50fcbedeb75ce3534889984296100a87f07e3
Gabe Williamson <gabe@stonefin.com>
2015-07-13 14:47:58 -0500

    whoopsies, delete the newly unused SnapperARguments.cs file

--- AutoSnapper/SnapperArguments.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AutoSnapper {
-  public class SnapperArguments
-  {
-    public bool SnapshotVolumes { get; set; }
-    public bool StartInstances { get; set; }
-    public bool StopInstances { get; set; }
-    public bool DisplaySummary { get; set; }
-  }
-}
